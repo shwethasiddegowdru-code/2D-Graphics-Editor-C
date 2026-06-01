82b55405c2e2a5eeee8140ccc3d7b4020faaafda
shwethasiddegowdru-code <shwethasiddegowdru@gmail.com>
2026-06-01 19:54:22 +0530

    Added canvas initialization and display

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define ROWS 20
+#define COLS 50
+
+char canvas[ROWS][COLS];
+
+void initializeCanvas()
+{
+    for(int i = 0; i < ROWS; i++)
+    {
+        for(int j = 0; j < COLS; j++)
+        {
+            canvas[i][j] = '_';
+        }
+    }
+}
+
+void displayCanvas()
+{
+    for(int i = 0; i < ROWS; i++)
+    {
+        for(int j = 0; j < COLS; j++)
+        {
+            printf("%c", canvas[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    initializeCanvas();
+    displayCanvas();
+
+    return 0;
+}
